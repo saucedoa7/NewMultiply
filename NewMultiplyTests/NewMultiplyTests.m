24b7ebe986757149bf5e32ae6da9354fa7c80881
Albert Saucedo <saucedoa7@gmail.com>
2014-09-12 12:43:24 -0500

    Initial Commit

---
+++ NewMultiplyTests/NewMultiplyTests.m
@@ -0,0 +1,34 @@
+//
+//  NewMultiplyTests.m
+//  NewMultiplyTests
+//
+//  Created by Albert Saucedo on 9/12/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NewMultiplyTests : XCTestCase
+
+@end
+
+@implementation NewMultiplyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
