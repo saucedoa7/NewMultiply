89d7863f43116cc9d5417698b0ae409633104ff0
Albert Saucedo <saucedoa7@gmail.com>
2014-09-12 13:31:20 -0500

    Finished MVP: Calculate the input number by the label number. Checks to see if the answer is divisible by 3 and/or 5 and check for fizz buzz

--- NewMultiply/ViewController.m
+++ NewMultiply/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *myMultiplier;
+@property (weak, nonatomic) IBOutlet UITextField *myNumber;
+@property (weak, nonatomic) IBOutlet UILabel *myAnswer;
 
 @end
 
@@ -20,10 +23,28 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (IBAction)onCalculateButtonPressed:(UIButton *)sender {
+    int textFieldNumber = [self.myNumber.text intValue];
+    int multiplierLabelNumber = [self.myMultiplier.text intValue];
+    int answerLabelInt = textFieldNumber * multiplierLabelNumber;
+    NSLog(@"%d", answerLabelInt);
+
+    self.myAnswer.text = [NSString stringWithFormat:@"%d", answerLabelInt];
+
+    if (answerLabelInt > 20) {
+        self.view.backgroundColor = [UIColor colorWithRed:0.33 green:0.84 blue:0.49 alpha:1];
+    } else {
+        self.view.backgroundColor = [UIColor colorWithRed:0.98 green:0.98 blue:0.98 alpha:1];
+    }
+
+    if (answerLabelInt % 3 == 0 && answerLabelInt % 5 == 0) {
+        self.myAnswer.text = @"FizzBuzz";
+    } else if (answerLabelInt % 3 == 0) {
+        self.myAnswer.text = @"Fizz";
+    } else if (answerLabelInt % 5 == 0){
+        self.myAnswer.text = @"Buzz";
+    }
 }
 
 @end
